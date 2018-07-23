95b02f3fd6f8c5cb6050307711511f0bf68923ca
blacksevenzqj <blackseven6696669>
2018-07-23 14:37:44 +0800

    123

--- ass-security-cas/src/main/java/ass/management/security/modules/business/test/controller/MyAdvisoryController.java
+++ ass-security-cas/src/main/java/ass/management/security/modules/business/test/controller/MyAdvisoryController.java
@@ -1,15 +1,28 @@
 package ass.management.security.modules.business.test.controller;
 
+import org.jasig.cas.client.authentication.AttributePrincipal;
+import org.jasig.cas.client.util.AssertionHolder;
+import org.jasig.cas.client.validation.Assertion;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.cas.authentication.CasAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/myAdvisory")
 public class MyAdvisoryController {
 
     @RequestMapping("/hello")
-    public String hello() {
+    public String hello(HttpServletRequest request) {
+        final CasAuthenticationToken token = (CasAuthenticationToken) request.getUserPrincipal();
+        AttributePrincipal principal = token.getAssertion().getPrincipal();
+        System.out.println(principal.getName());
+        Map<String, Object> map = principal.getAttributes();
+
         return "不验证哦";
     }
 
