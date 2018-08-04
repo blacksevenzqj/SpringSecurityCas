9423eb0c5ad715570ca3ce2a19997f1d7680541d
blacksevenzqj <397353721@qq.com>
2018-08-05 02:38:37 +0800

    first

--- dyzhsw-common/src/main/java/ass/management/common/utils/JConstants.java
+++ dyzhsw-common/src/main/java/ass/management/common/utils/JConstants.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package ass.management.utils;
+package ass.management.common.utils;
 
 import java.nio.charset.Charset;
 import java.nio.charset.UnsupportedCharsetException;
