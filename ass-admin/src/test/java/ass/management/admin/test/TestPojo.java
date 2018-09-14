67b37c8be2cc41002fd07744cb9e4debe7cb967f
blacksevenzqj <397353721@qq.com>
2018-09-15 02:27:40 +0800

    home

--- ass-admin/src/test/java/ass/management/admin/test/TestPojo.java
+++ ass-admin/src/test/java/ass/management/admin/test/TestPojo.java
@@ -6,11 +6,17 @@ import ass.management.admin.modules.sys.shiro.ShiroUtils;
 public class TestPojo {
 
     public static void main(String[] args){
-        SysUserEntity sysUserEntity = new SysUserEntity();
-        System.out.println(sysUserEntity);
-        String str = "123456";
-        String salt = "YzcmCZNvbXocrsz9dm8e";
-        System.out.println(ShiroUtils.sha256(str, salt));
+//        SysUserEntity sysUserEntity = new SysUserEntity();
+//        System.out.println(sysUserEntity);
+//        String str = "123456";
+//        String salt = "YzcmCZNvbXocrsz9dm8e";
+//        System.out.println(ShiroUtils.sha256(str, salt));
+
+        int i = 253;
+        System.out.println(Integer.toBinaryString(i));
+        int a = i << 2;
+        System.out.println(Integer.toBinaryString(a));
+
     }
 
 }
